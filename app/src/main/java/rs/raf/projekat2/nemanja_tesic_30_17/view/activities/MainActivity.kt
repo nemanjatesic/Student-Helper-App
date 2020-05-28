3051b69576e01e8c30cf7c6e7933095b8983f698
nemanjatesic <33559264+nemanjatesic@users.noreply.github.com>
2020-05-28 18:11:41 +0200

    Initial commit

---
+++ app/src/main/java/rs/raf/projekat2/nemanja_tesic_30_17/view/activities/MainActivity.kt
@@ -0,0 +1,11 @@
+package rs.raf.projekat2.nemanja_tesic_30_17.view.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/rs/raf/projekat2/nemanja_tesic_30_17/view/activities/SplashActivity.kt
@@ -0,0 +1,2 @@
+package rs.raf.projekat2.nemanja_tesic_30_17.view.activities
+
